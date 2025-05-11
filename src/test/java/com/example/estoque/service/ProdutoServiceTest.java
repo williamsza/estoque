3d33bd621e539c16218eff110577d796ddac3989
williamsza <wyllsza@gmail.com>
2025-05-11 13:10:48 -0300

    Criação da estrutura de testes e do primeiro teste cadastrarProduto

---
+++ src/test/java/com/example/estoque/service/ProdutoServiceTest.java
@@ -0,0 +1,24 @@
+package com.example.estoque.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProdutoServiceTest {
+
+    @Test
+    void cadastrarProduto() {
+    }
+
+    @Test
+    void encontrarTodos() {
+    }
+
+    @Test
+    void atualizarEstoque() {
+    }
+
+    @Test
+    void encontrarPorNome() {
+    }
+}
